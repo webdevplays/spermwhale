874443d987519b3ba63102a4cc4c4de9d0d8a7a6
Personal-use <elthrone1233@gmail.com>
2025-12-29 21:05:24 +0800

    update

---
+++ _next/static/ghUVHNqA9DVNdph95XfQO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/931-39005e575de9ae01.js","static/chunks/pages/index-66f087ddc6e8d8cc.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
